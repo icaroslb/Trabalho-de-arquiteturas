cf0b530104396f13bbc2ba7a8b4f570a26b1fa70
Hariamy Vasconcelos <hariamymv@hotmail.com>
2018-05-04 23:58:19 -0300

    É o assembler2.0.py Só renomeei as paradas

--- assembler.py
+++ assembler.py
@@ -1,209 +1,248 @@
-import sys      # Biblioteca usada para receber argumentos da linha de comando.
+import sys
 
-# Dicionário que contém as instruções sem operando. #
+""" Dicionário que contém as instruções
+    Formato: { mnemônico: [hex, [operandos]]}
+    Tipos de operandos: const1, const2, var, new_var, label """
 
-no_operand_dict = { 'nop':    0x01,     'iadd':          0x02,    'isub':   0x05,     'iand':      0x08,
-                    'ior':    0x0b,     'dup':           0x0e,    'pop':    0x10,     'swap':      0x13,
-                    'wide':   0x28,     'ireturn':       0x6b }
+instructions_dict = {
+                        'nop':                  [0x01, []],
+                        'iadd':                 [0x02, []],
+                        'isub':                 [0x05, []],
+                        'iand':                 [0x08, []],
+                        'ior':                  [0x0b, []],
+                        'dup':                  [0x0e, []],
+                        'pop':                  [0x10, []],
+                        'swap':                 [0x13, []],
+                        'bipush':               [0x19, ['byte']],
+                        'iload':                [0x1c, ['varnum']],
+                        'istore':               [0x22, ['new_varnum']],
+                        'wide':                 [0x28, []],
+                        'ldc_w':                [0x32, ['index']],          # Não entendo como funciona
+                        'iinc':                 [0x36, ['varnum', 'const']],
+                        'goto':                 [0x3c, ['offset']],
+                        'iflt':                 [0x43, ['offset']],
+                        'ifeq':                 [0x47, ['offset']],
+                        'if_icmpeq':            [0x4b, ['offset']],
+                        'invokevirtual':        [0x55, ['disp']],           # Não entendo como funciona
+                        'ireturn':              [0x6b, []]
+                    }
 
-# Dicionário que contém as instruções com operando(s). #
+# Estruturas e constantes globais #
 
-operand_dict = {    'goto':   0x3c,     'iftl':          0x43,    'ifeq':   0x47,     'if_icmpeq': 0x4b,   # Desvio.
-                    'ldc_w':  0x32,     'invokevirtual': 0x55,                                             # Operando 2 bytes.
-                    'bipush': 0x19,     'iload':         0x1c,    'istore': 0x22,     'iinc':      0x36 }  # Operando 1 byte.
+labels_dict = {}        # Dicionário que armazena as labels e o byte que cada uma indica
+variables_dict = {}     # Dicionário que armazena as variáveis e suas posições de memória
+byte_list = []          # Lista que armazena os bytes que serão adicionados ao arquivo
+error_log = ''          # String que armazena o log de erros
 
-# Declaração das estruturas de dados utilizadas. #
+byte_counter = 0        # Conta o programa byte a byte
+line_counter = 0        # Conta o programa linha por linha
 
-labels_list = {}        # Dicionário que armazena as labels e suas respectivas posições (em bytes).
-constants_list = {}     # Dicionário que armazena as constantes e suas posições de memória (em bytes).
-byte_list = []          # Lista que armazena os bytes que serão adicionados ao arquivo.
-error_log = ''          # String que armazena o log de erros do programa.
-
-# Declaração dos contadores globais. #
-
-byte_counter = 0    # Contador global de bytes.
-next_constant = 0   # Contador global de constantes.
-
-# Função main. #
+next_variable = 1       # Indica a posição de memória da pŕoxima variável a ser adicionada
 
 def main():
-    global byte_counter
+    global line_counter
 
-    with open(sys.argv[1]) as program:      # Abre o arquivo de acordo com o argumento passado pela linha de comandos.
-        line_counter = 0
+    try:
+        program = open(sys.argv[1], 'r')
+    except:
+        print('no argument')
+        raise IndexError
 
-        for line in program:
-            line_counter += 1
+    for line in program:
+        line_counter += 1
 
-            sline = line.lower().split()        # Separa os strings da linha que não são espaços em braco e os armazena em sline.
-            
-            if sline != [] and not is_comment(sline[0]):        # Se linha vazia ou comentário, o programa segue para a próxima linha.
+        s_line = line.lower().split()                                           # Separa os strings em uma lista
+
+        try:
+            if is_label(s_line[0]):                                         # Verifica se a linha contém uma label
+                add_label(s_line[0])                                        # Se sim, cria a label
+                del s_line[0]
+        
+        except IndexError:
+            pass
+        
+
+        if s_line != [] and not is_comment(s_line[0]):                          # Verifica se linha vazia ou comentário
+            instruction = s_line[0]
+
+            if is_valid_instruction(instruction):
+                operand_types = instructions_dict[instruction][1]
+                number_of_operands = len(operand_types)
+                operand = s_line[1 : number_of_operands + 1]
+
+                if is_valid_operand(instruction, operand, operand_types, number_of_operands):
+                    add_instruction(instruction)
+                    add_operand(instruction, operand, operand_types, number_of_operands)
+
+                else:
+                    add_error('invalid_operand', line_counter)
+
+            else:
+                add_error('invalid_instruction', line_counter)
                 
-                if is_label(sline[0]):      # Verifica se a linha contém uma label.
-                    add_label(sline[0])
-                    del sline[0]
-
-                if is_valid_instruction(sline[0]):      # Verifica se a instrução é válida.
-                    add_instruction(sline[0])
-
-                else:
-                    add_error('invalid_instruction', line_counter)
-                    
-                if is_valid_operand(sline):     # Verifica se o(s) operando(s) é(são) válido(s).
-                    if sline[0] in operand_dict:
-                        add_operand(sline)
-
-                else:
-                    if ('Line: %02d' %line_counter) not in error_log:
-                        add_error('invalid_operand', line_counter)
-
-    if error_log == '':    
-        generate_file()     # Gera o arquivo final.
+    program.close()
     
+    if error_log == '':    
+        generate_file()
+
     else:
         print (error_log)
 
-# Escopo das funções. #
+# Escopo das funções #
 
-def is_comment(string):              # Informa se uma string é um comentário.
-    return string.startswith('//')
+def is_comment(string):
+    """Informa se uma string é um comentário"""
+    try:
+        return string.startswith('//')
+    except TypeError:
+        raise TypeError
 
-def is_label(string):                # Informa se uma string é uma label.
-
-    return (not(string in no_operand_dict.keys())) and (not(string in operand_dict.keys()))
-
-def add_label(label):                # Adiciona uma label ao vetor byte_list.
-    global byte_counter
-
-    labels_list[label] = byte_counter + 1
-
-def is_valid_instruction(string):    # Informa se uma instrução é válida.
-    return (string in no_operand_dict.keys()) or (string in operand_dict.keys())
-
-def add_instruction(instruction):    # Adiciona o endereço de uma instrução ao byte_list.
-    global byte_counter
-    byte_counter += 1
-
-    if instruction in no_operand_dict.keys():  # Instruções sem operando.
-        byte_list.append(no_operand_dict[instruction])
-
-    else:                            # Instruções com operando(s).
-        byte_list.append(operand_dict[instruction])
-
-def is_valid_constant(string):       # Informa se uma string é uma constante válida.
-    return (len(string) > 0) and (string[0].isalpha())
-
-def is_valid_operand(sline):         # Informa se uma linha possui operando(s) válido(s).
-
-    if sline[0] in no_operand_dict.keys():     # Instruções sem operando, espera-se ausência de operandos.
-        return (len(sline) == 1) or (is_comment(sline[1]))
+def is_label(string):
+    """Informa se uma string é uma label"""
+    global line_counter, instructions_dict, variables_dict
     
-    elif sline[0] in operand_dict.keys():   # Instruções com operando.
-        if (len(sline) > 1) and (not is_comment(sline[1])):
-            
-            if (sline[0] == 'goto') or (sline[0] == 'iflt') or (sline[0] == 'ifeq') or (sline[0] == 'if_icmpeq'):
-                return ((len(sline) == 2) or (is_comment(sline[2]))) and (is_label(sline[1]))
-            
-            elif (sline[0] == 'bipush') or (sline[0] == 'ldc_w') or (sline[0] == 'invokevirtual'):
-                return ((len(sline) == 2) or (is_comment(sline[2]))) and (sline[1].isnumeric())
-            
-            elif (sline[0] == 'iload'):
-                return ((len(sline) == 2) or (is_comment(sline[2]))) and (sline[1] in constants_list)
-            
-            elif (sline[0] == 'istore'):
-                return ((len(sline) == 2) or (is_comment(sline[2]))) and (is_valid_constant(sline[1]))
-            
-            else: # (sline == 'iinc')
-                if (len(sline) > 2) and (not is_comment(sline[2])):
-                    return ((len(sline) == 3) or (is_comment(sline[3]))) and (sline[1].isnumeric()) and (is_valid_constant(sline[2]))
+    if string in variables_dict.keys():
+        add_error('label_same_name_as_variable', line_counter)
+        return False
 
+    else:
+        return (string not in instructions_dict.keys()) and string.replace("_","").isalnum()
+
+def add_label(label):
+    """Adiciona uma label ao dicionário labels_dict"""
+    global byte_counter, labels_dict
+
+    labels_dict[label] = byte_counter + 1
+
+def is_valid_instruction(string):
+    """Informa se uma string é uma instrução é válida"""
+    global instructions_dict
+
+    return string in instructions_dict.keys()
+
+def add_instruction(instruction):
+    """Adiciona o endereço de uma instrução ao byte_list"""
+    global byte_counter, instructions_dict
+
+    byte_counter += 1
+    byte_list.append(instructions_dict[instruction][0])
+
+def is_valid_variable(string):
+    """Informa se uma string é uma variável válida"""
+    global line_counter, labels_dict
+
+    if string in labels_dict.keys():
+        add_error('varialbe_same_name_as_label', line_counter)
+        return False
+
+    else:
+        return string[0].isalpha() and string.replace("_","").isalnum()
+
+def is_valid_operand(instruction, operand, operand_types, number_of_operands):
+    """Informa se uma linha possui operando(s) válido(s)."""
+    global variables_dict
+
+    if number_of_operands == len(operand):
+        if number_of_operands == 0:
+            return True
+        
+        for i in range (0, number_of_operands, 1):
+            
+            if operand_types[i] == 'varnum':
+                
+                return operand[i] in variables_dict.keys()
+
+            elif operand_types[i] == 'new_varnum':
+                
+                return is_valid_variable(operand[i])
+
+            elif (operand_types[i] == 'byte') or (operand_types[i] == 'const') or (operand_types[i] == 'disp') or (operand_types[i] == 'index'):
+                return operand[i].isnumeric()
+
+            else:  # operand_types[i] == 'offset':
+                
+                return is_label(operand[i])
+    
     else:
         return False
 
-def add_operand(sline):              # Adiciona o endereço de um operando ao byte_list.
-    global byte_counter
-    global next_constant
+def add_operand(instruction, operand, operand_types, number_of_operands):
+    """Adiciona o endereço de um operando ao byte_list"""
+    global byte_counter, next_variable, variables_dict
 
-    if (sline[0] == 'goto') or (sline[0] == 'iflt') or (sline[0] == 'ifeq') or (sline[0] == 'if_icmpeq'):
-        byte_list.append([sline[1], byte_counter])
-        byte_counter += 2
-    
-    elif (sline[0] == 'bipush'):
-        byte_list.append(int(sline[1]))
-        byte_counter += 1
-    
-    elif (sline[0] == 'iload'):
-        byte_list.append(constants_list[sline[1]])
-        byte_counter += 1
-    
-    elif (sline[0] == 'istore'):
-        if sline[1] not in constants_list.keys():
-            constants_list[sline[1]] = next_constant
-            next_constant += 1
-        byte_list.append(constants_list[sline[1]])
-        byte_counter += 1
+    for i in range (0, number_of_operands, 1):
+        if operand_types[i] == 'varnum':
+            byte_list.append(variables_dict[operand[i]])
+            byte_counter += 1
 
-    elif (sline[0] == 'ldc_w') or (sline[0] == 'invokevirtual'):
-        byte_list.append(int(sline[1]) & 0xFF)
-        byte_list.append(int(sline[1]) >> 8)
-        byte_counter += 2
-    
-    else: # (sline == 'iinc')
-        byte_list.append(int(sline[1]))
-        byte_list.append(constants_list[sline[2]])
-        byte_counter += 2
+        elif operand_types[i] == 'new_varnum':
+            if operand[i] not in variables_dict.keys():
+                variables_dict[operand[i]] = next_variable
+                next_variable += 1
 
-def add_error(error_type, error_line): # Adiciona um erro ao log de erros.
+            byte_list.append(variables_dict[operand[i]])
+            byte_counter += 1
+
+        elif (operand_types[i] == 'byte') or (operand_types[i] == 'const'):
+            byte_list.append(int(operand[i]))
+            byte_counter += 1
+
+        elif (operand_types[i] == 'disp') or (operand_types[i] == 'index'):
+            byte_list.append(int(operand[i]) & 0xff)
+            byte_list.append(int(operand[i]) >> 8)
+            byte_counter += 2
+
+        else:  # operand_types[i] == 'offset':
+            """Cria uma lista [label, endereço] que será substituída na função generate_file()"""
+            byte_list.append([operand[i], byte_counter])
+            byte_counter += 2
+
+def add_error(error_type, error_line):
+    """Adiciona erros ao log"""
     global error_log
 
-    error_to_be_added = 'Line: ' + format(error_line, '#02d') + ' error: ' + str(error_type) + '\n'
-    error_log = error_log + error_to_be_added
+    if (str(error_line) not in error_log):
+        error_to_be_added = 'Line: ' + format(error_line, '#02d') + ' error: ' + str(error_type) + '\n'
+        error_log = error_log + error_to_be_added
 
+def generate_file():
+    """Gera o arquivo final"""
+    global byte_list, byte_counter, line_counter
 
-def generate_file(): # Gera o arquivo final.
-    global byte_list
-    global byte_counter
+    final_byte_array = bytearray()      # Bytearray que será gravado no arquivo
 
-    final_byte_array = bytearray()      # Bytearray que será gravado no arquivo.
-   
-    # Escrevendo os 4 bytes de tamanho do programa. 
-    final_byte_array.append((byte_counter + 20) & 0xff)
-    final_byte_array.append(((byte_counter + 20) >> 8) & 0xff)
-    final_byte_array.append(((byte_counter + 20) >> 16) & 0xff)
-    final_byte_array.append(((byte_counter + 20) >> 24) & 0xff)
-    
-    # Inicialização da memória.
-    bytes_to_be_added = [0x00, 0x73, 0x00, 0x00, # memory[1]  = 0x0073
-                         0x06, 0x00, 0x00, 0x00, # memory[4]  = 0x0006
-                         0x01, 0x10, 0x00, 0x00, # memory[8]  = 0x1001
-                         0x00, 0x04, 0x00, 0x00, # memory[12] = 0x0400
-                         0x01, 0x10, 0x00, 0x00] # memory[16] = 0x1001
+    # Quantidade de bytes do programa
+    program_size = (byte_counter + 20).to_bytes(4, byteorder = 'little', signed = True)
+    final_byte_array += program_size
 
-    bytes_to_be_added[16] = (0x1001 + next_constant) & 0xff
-    bytes_to_be_added[17] = ((0x1001 + next_constant)>>8) & 0xff
-    bytes_to_be_added[18] = ((0x1001 + next_constant)>>16) & 0xff
-    bytes_to_be_added[19] = ((0x1001 + next_constant)>>24) & 0xff
+    # Inicialização da memória
+    bytes_to_be_added = [   
+                            0x7300,
+                            0x0006,
+                            0x1001,
+                            0x0400,
+                            0x1001 + len(variables_dict.keys())
+                        ]
 
     for byte in bytes_to_be_added:
-        final_byte_array.append(byte)          # Escreve a inicialização da memória.
+        final_byte_array += byte.to_bytes(4, byteorder = 'little', signed = True)
    
-    for byte in byte_list:                                         # Escreve os bytes das instruções, labels e operandos.
-        try:
-            if type(byte) is list:                                     # Verifica se é label e adiciona 2 bytes relativos.
-                label_byte = labels_list[byte[0]] - byte[1]            # Valor da label será a posição relativa da label à sua linha destino.
-                final_byte_array.append(label_byte & 0xFF)
-                final_byte_array.append(label_byte >> 8)
-
+    # Bytes das instruções, operandos e labels
+    for byte in byte_list:
+        if type(byte) is list:                                     # Verifica se é label
+            if byte[0] not in labels_dict.keys():                  # Verifica se a label existe
+                    add_error('label_not_found', line_counter)
+                
             else:
-                final_byte_array.append(byte)                          # Se não for label, apenas adiciona o byte ao bytearray.
-        except:
-            print("Erro 404: not label found")
+                label_byte = labels_dict[byte[0]] - byte[1] 
+                final_byte_array += label_byte.to_bytes(2, byteorder = 'big', signed = True)
 
-    # print(final_byte_array)    # Mostra o bytearray antes de finalizar.
-    
-    final_program = open(sys.argv[1][:-4] + '.exe', 'wb')   # Abre o arquivo para escrita.
-    final_program.write(final_byte_array)                   # Escreve.
+        else:
+            final_byte_array.append(byte)
+
+    final_program = open(sys.argv[1][:-4] + '.exe', 'wb')
+    final_program.write(final_byte_array)
     final_program.close()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
