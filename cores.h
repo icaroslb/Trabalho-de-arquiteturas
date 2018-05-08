0e62ef40c93e45a48a8f980a1b2b2f2262a17e52
Hariamy Vasconcelos <hariamymv@hotmail.com>
2018-05-08 09:06:44 -0300

    Da as corzinhas bonitinhas

---
+++ cores.h
@@ -0,0 +1,36 @@
+#ifndef __CORES_TRABALHO
+
+#define COR_NORMAL  	"\x1B[0m"
+#define COR_VERMELHO  	"\x1B[31m"
+#define COR_VERDE       "\x1B[32m"
+#define COR_AMARELO  	"\x1B[33m"
+#define COR_AZUL        "\x1B[34m"
+#define COR_MAGENTA     "\x1B[35m"
+#define COR_CIANO       "\x1B[36m"
+#define COR_BRANCO      "\x1B[37m"
+#define COR_PRETO       "\x1B[30m"
+
+#define FUNDO_PRETO     "\x1B[40m"
+#define FUNDO_VERMELHO	"\x1B[41m"
+#define FUNDO_VERDE     "\x1B[42m"
+#define FUNDO_AMARELO	"\x1B[43m"
+#define FUNDO_AZUL      "\x1B[44m"
+#define FUNDO_MAGENTA	"\x1B[45m"
+#define FUNDO_CIANO     "\x1B[46m"
+#define FUNDO_BRANCO	"\x1B[47m"
+#define FUNDO_NORMAL	"\x1B[49m"
+
+#define __CORES_TRABALHO
+#endif
+
+/**********************
+ 
+ EXEMPLO DE UTILIZAÇÃO
+
+ printf("Entre a sua jogada, composta de cinco cores separadas por vírgula, 
+ usando o código a seguir:\n%s - vm = vermelho\n%s - vd = verde\n%s - am = amarelo\n%s - 
+ az = azul\n%s - mg = magenta\n%s - cn = ciano\n\n%sEntre sua jogada: ", COR_VERMELHO, 
+ COR_VERDE, COR_AMARELO, COR_AZUL, COR_MAGENTA, COR_CIANO, COR_NORMAL);
+ 
+ */
+
