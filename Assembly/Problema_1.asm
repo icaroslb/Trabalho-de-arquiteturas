c68da684c1c7adc9d143545b6ad28efcb7c7e041
Hariamy <hariamymv@hotmail.com>
2018-06-21 09:44:13 -0300

    Comentários detalhados

--- Assembly/Problema_1.asm
+++ Assembly/Problema_1.asm
@@ -7,12 +7,12 @@ mov ds, ax
 inicio:	
 	cli
 
-	mov ah, 0
+	mov ah, 0	 ;Muda o modo de vídeo
 	mov al, 0x13 ;modo gráfico 320x200x8bits (256 cores)
 	int 0x10
 
 	int 0x13
-	mov ah, 0x02 ;ler setores
+	mov ah, 0x02;ler setores
 	mov al, 32  ;Hariamy: Lendo 32 setores que é o tamanho da imagem ;quantos setores devem ser lidos 32
 	mov cl, 2  ;ler a partir do setor 2
 	mov ch, 0  ;do cilindro 0
@@ -21,26 +21,25 @@ inicio:
 	int 0x13
 
 	;move imagem para memória de vídeo
-	mov ax, 0xA000
-	mov es, ax
-	mov di, 0
-	mov si, 0x7e00;Onde a imagem tá salva
+	mov ax, 0xA000 ;Endereço da memória de vídeo
+	mov es, ax	   ;Coloca 0xA000 no registrador para se o setor
+	mov di, 0	   ;Coloca o índice 0 no índice do setor da imagem
+	mov si, 0x7e00 ;Onde a imagem tá salva
 	
-.loop2:	
-	mov al, [ds:si]
-	mov [es:di], al
+.copiar:	
+	mov al, [ds:si] ;Move o conteúdo do setor 0 índice si para o registrador al
+	mov [es:di], al ;Move o registrador al para o conteúdo do setor A000 índice di
 
-;	EXIBIR LENTAMENTE
-	;mov ah, 0x0E
-	;mov al, 't'
-	;int 0x10
-;	FIM EXIBIR LENTAMENTE
-
+;	Se o valor de di for igual ao tamanho da ímagem
 	cmp di, 16000
-	je .final
-	inc si
+	 
+;	Então pula para o final
+	je .final 
+
+;	Senão, incrementa o si e o di
+	inc si 
 	inc di
-	jmp .loop2
+	jmp .copiar
 
 .final:
 	jmp .final
